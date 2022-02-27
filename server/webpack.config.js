f38740d175e4ca1066f20c0cffe60a065c2897d0
kyle smith <kyle@someotherkyle.com>
2022-02-27 10:23:24 -0500

    prettier'd config.webpack.js

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,18 +1,18 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development",
-    entry : {
-        index: './src/index.js'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Output Management',
-        }),
-    ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
-}
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
