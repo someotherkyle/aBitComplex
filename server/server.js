9fb582191ab312b31923d0d07b9cf14021a03639
kyle smith <kyle@someotherkyle.com>
2022-03-06 17:31:45 -0500

    finished back end baseline... lots to do

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const MongoClient = require('mongodb').MongoClient
 const bodyParser = require('body-parser')
+const db = require('./config/db')
 
 const app = express()
 const listenPort = 8080
+
 app.use(bodyParser.urlencoded({ extended: true }))
-require('./app/routes')(app, {})
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err)
+  const deebs = database.db('aBitComplex')
+  require('./app/routes')(app, deebs)
+})
 app.listen(listenPort, () => console.log(`We are live on ${listenPort}.`))
