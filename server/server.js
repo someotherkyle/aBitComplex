fc38810aff37c51e8a7c954e7a70f2b0aea55a0d
kyle smith <kyle@someotherkyle.com>
2022-03-12 09:04:13 -0500

    using .env instaed of config/db.js

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
+require('dotenv').config({ path: '../.env' })
 const express = require('express')
 const MongoClient = require('mongodb').MongoClient
 const bodyParser = require('body-parser')
-const db = require('./config/db')
 
 const app = express()
 const listenPort = 8080
 
 app.use(bodyParser.urlencoded({ extended: true }))
-MongoClient.connect(db.url, (err, database) => {
-  if (err) return console.log(err)
-  const deebs = database.db('aBitComplex')
-  require('./app/routes')(app, deebs)
+MongoClient.connect(process.env.MONGO_URL, (err, database) => {
+  if (err) return console.error(err)
+  const db = database.db('aBitComplex')
+  require('./app/routes')(app, db)
 })
 app.listen(listenPort, () => console.log(`We are live on ${listenPort}.`))
